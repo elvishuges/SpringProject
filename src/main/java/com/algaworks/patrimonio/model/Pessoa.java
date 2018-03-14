dc6ac0f2538aef23d31043aeca0f7eb5b15f6b37
elvishuges <Elvis@elvis.com>
2018-03-13 23:47:38 -0300

    mudancas entidade pessoa

--- src/main/java/com/algaworks/patrimonio/model/Pessoa.java
+++ src/main/java/com/algaworks/patrimonio/model/Pessoa.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.GenericGenerator;
 
@@ -19,7 +20,7 @@ public class Pessoa {
 	@NotEmpty
 	private String nome;
 	
-	@NotEmpty
+	@NotNull
 	private int idade;
 
 	@Override
